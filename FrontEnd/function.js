eb80e9eb00ece876b22e035d5324765bdfb44e53
Damien TAILLEMAUD <taillemaudamien87@gmail.com>
2025-08-06 22:59:32 +0200

    Ajout des boutons de catégorie dynamiques au-dessus de la galerie

--- FrontEnd/function.js
+++ FrontEnd/function.js
@@ -9,6 +9,28 @@ export async function getData(url){
     }
 }
 
+//Fonction de création de balise
+function createBalise(balise, id, text, src, alt){
+        const element = document.createElement(balise);
+        if (id){
+            element.id = id;
+        }
+
+        if (text){
+            element.innerText = id;
+        }
+
+        if (src){
+            element.src = src;
+        }
+
+        if (alt){
+            element.alt = alt;
+        }
+
+        return element;
+}
+
 
 export async function generateWorks(){
     const getWorks = await getData("http://localhost:5678/api/works");
@@ -36,6 +58,42 @@ export async function generateWorks(){
         figure.appendChild(img);
         figure.appendChild(figcaption);
         divGallery.appendChild(figure);
-
     }
+
+    //Je récupére les categories de tout les projets dans un tableau
+    const categoriesWork = getWorks.map(work => work.category.name);
+    console.log(categoriesWork);
+
+    //Ici je converti mon Set(categoriesWork) qui me construit un objet avec les valeurs uniques du tableau categorieWork
+    //Puisque c'est un objet je le converti en tableau pour le parcourir plus facilement
+    const categories = [...new Set(categoriesWork)];
+    console.log(categories);
+
+
+    //Section dans laquelle je vais rajouter la div où il y aura mes boutons
+    const sectionPortfolio = document.getElementById('portfolio');
+
+    //Creation de ma div qui va contenir mes boutons, avec la classe list-categorie
+    const divCategorie = document.createElement('div')
+    divCategorie.classList.add('list-categorie');
+
+    sectionPortfolio.insertBefore(divCategorie, divGallery);
+
+    //Création du bouton "Tous"
+    const btnTous = createBalise('button', 'Tous', 'Tous');
+    divCategorie.appendChild(btnTous);
+
+
+    //Boucle foreach pour créé des boutons pour chaque catégorie
+    categories.forEach(category => {
+        
+        //Création du bouton et ajout du text et d'un id pour chaque bouton
+        const btnCategories = createBalise('button', category, category)
+        btnCategories.classList.add('btn-categories');
+
+        //Intégration de chaque bouton dans la div créée auparavant
+        divCategorie.appendChild(btnCategories);
+
+    });
 }
+
